0609d6ec672a7894fa2c6df34bdcfa0f603c819b
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-04-25 22:48:28 +0300

    fix config installation of w3m

--- scripts/set_settings.sh
+++ scripts/set_settings.sh
@@ -54,6 +54,9 @@ fi
 git clone https://github.com/Lekensteyn/qt5printers.git $HOME_DIR/.gdb/qt5printers
 
 echo Install config for w3m
+if [ ! -d $HOME_DIR/.w3m ]; then
+  mkdir $HOME_DIR/.w3m
+fi
 cp $FILE_DIR/../w3m/* $HOME_DIR/.w3m
 
 cd $CUR_DIR
