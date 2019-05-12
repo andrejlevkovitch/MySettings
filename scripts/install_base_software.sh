e6e708325f8c4addb8cf33cca0245c63dff0a197
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-05-12 15:30:46 +0300

    fix problem with installation on debian buster

--- scripts/install_base_software.sh
+++ scripts/install_base_software.sh
@@ -8,12 +8,14 @@ echo --------------------------------------------------------------------------
 if [ "$CUR_SYSTEM" = "ubuntu" ]; then
   echo add testing ppa
   add-apt-repository ppa:ubuntu-toolchain-r/test
-elif [ "$CUR_SYSTEM" = "debian" ] && [ "$(lsb_release -cs)" != "buster" ]; then
-  echo upgrade debian to buster
-  sed -i "s/$(lsb_release -cs)/buster/g" /etc/apt/sources.list
-  apt-get update
-  apt-get upgrade -y
-  apt-get dist-upgrade -y
+elif [ "$CUR_SYSTEM" = "debian" ]; then
+  if [ "$(lsb_release -cs)" != "buster" ]; then
+    echo upgrade debian to buster
+    sed -i "s/$(lsb_release -cs)/buster/g" /etc/apt/sources.list
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+  fi
 else
   echo unsupported system: $CUR_SYSTEM
   exit 1
