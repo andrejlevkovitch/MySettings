405b8905528f47b24446c41649d022142f6051a3
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-05-12 15:50:37 +0300

    add removing downloaded files when faild installation. And add check ctags download

--- scripts/install_dev_soft_from_src.sh
+++ scripts/install_dev_soft_from_src.sh
@@ -54,6 +54,7 @@ if [ "$CUR_SYSTEM" = "debian" ]; then
   wget "http://checkinstall.izto.org/files/source/checkinstall-1.6.2.tar.gz"
   echo "dc61192cf7b8286d42c44abae6cf594ee52eafc08bfad0bea9d434b73dd593f4  checkinstall-1.6.2.tar.gz" | sha256sum -c | grep -v OK
   if [ $? -eq 0 ]; then
+    rm checkinstall-1.6.2.tar.gz 
     echo faild download checkinstall
     exit 1
   fi
@@ -61,8 +62,15 @@ if [ "$CUR_SYSTEM" = "debian" ]; then
   tar -xzvf checkinstall-1.6.2.tar.gz
   rm checkinstall-1.6.2.tar.gz
   cd checkinstall-1.6.2
+  make
   make install
   checkinstall -D -y --pkgname=checkinstall-ch --pkgversion=1.6.2 --nodoc --backup=no
+  if [ $? -ne 0 ]; then
+    cd ..
+    rm -rf checkinstall-1.6.2
+    echo checkinstall can not be installed
+    exit 1
+  fi
   cd ..
   rm -rf checkinstall-1.6.2
 else
@@ -74,6 +82,7 @@ echo Install boost
 wget "https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz"
 echo "9a2c2819310839ea373f42d69e733c339b4e9a19deab6bfec448281554aa4dbb boost_1_69_0.tar.gz" | sha256sum -c | grep -v OK
 if [ $? -eq 0 ]; then
+  rm boost_1_69_0.tar.gz
   echo boost can not be loaded
   exit 1
 fi
@@ -86,6 +95,8 @@ cd boost_1_69_0
 ./b2
 checkinstall -D -y --pkgname=boost-ch --pkgversion=1.69.0 --nodoc --backup=no ./b2 install
 if [ $? -ne 0 ]; then
+  cd ..
+  rm -rf boost_1_69_0
   echo boost can not be installed
   exit 1
 fi
@@ -99,6 +110,7 @@ echo Install cmake
 wget "https://github.com/Kitware/CMake/releases/download/v3.14.1/cmake-3.14.1.tar.gz"
 echo "7321be640406338fc12590609c42b0fae7ea12980855c1be363d25dcd76bb25f  cmake-3.14.1.tar.gz" | sha256sum -c | grep -v OK
 if [ $? -eq 0 ]; then
+  rm cmake-3.14.1.tar.gz
   echo cmake can not be loaded
   exit 1
 fi
@@ -111,6 +123,8 @@ cd cmake-3.14.1
 make -j4
 checkinstall -D -y --pkgname=cmake-ch --pkgversion=3.14.1 --nodoc --backup=no
 if [ $? -ne 0 ]; then
+  cd ..
+  rm -rf cmake-3.14.1
   echo cmake can not be installed
   exit 1
 fi
@@ -123,6 +137,13 @@ echo --------------------------------------------------------------------------
 # need for vim (tagbar)
 echo Install universal ctags
 wget "https://github.com/universal-ctags/ctags/archive/master.zip"
+echo "8b38a75cf3dccf7728787cacacacca1f24efc5b2c50a5e5004e19e86852504be  master.zip" | sha256sum -c | grep -v OK
+if [ $? -eq 0 ]; then
+  rm master.zip
+  echo universal ctags can not be loaded
+  exit 1
+fi
+
 unzip master.zip
 rm master.zip
 
@@ -133,6 +154,8 @@ make -j4
 # real version of universal-ctags is 0.0.0, but we can not set it as version in checkinstall
 checkinstall -D -y --pkgname=ctags-ch --pkgversion=0.0.1 --nodoc --backup=no
 if [ $? -ne 0 ]; then
+  cd ..
+  rm -rf ctags-master
   echo ctags can not be installed
   exit 1
 fi
@@ -146,6 +169,7 @@ echo Install vim
 wget "https://github.com/vim/vim/archive/v8.1.1140.tar.gz"
 echo "b2bd214f9e562308af7203e3e8cfeb13327d503ab2fe23090db9c42f13ca0145  v8.1.1140.tar.gz" | sha256sum -c | grep -v OK
 if [ $? -eq 0 ]; then
+  rm v8.1.1140.tar.gz
   echo vim can not be loaded
   exit 1
 fi
@@ -170,6 +194,8 @@ cd vim-8.1.1140
 make -j4
 checkinstall -D -y --pkgname=vim-ch --pkgversion=8.1.1140 --nodoc --backup=no
 if [ $? -ne 0 ]; then
+  cd ..
+  rm -rf vim-8.1.1140
   echo vim can not be installed
   exit 1
 fi
@@ -183,6 +209,7 @@ echo Install vifm
 wget "https://github.com/vifm/vifm/archive/v0.10.tar.gz"
 echo "e5681c9e560e23d9deeec3b5b12e0ccad82612d9592c00407f3dd75cf5066548  v0.10.tar.gz" | sha256sum -c | grep -v OK
 if [ $? -eq 0 ]; then
+  rm v0.10.tar.gz
   echo vifm can not be loaded
   exit 1
 fi
@@ -195,6 +222,8 @@ cd vifm-0.10
 make -j4
 checkinstall -D -y --pkgname=vifm-ch --pkgversion=0.10 --nodoc --backup=no
 if [ $? -ne 0 ]; then
+  cd ..
+  rm -rf vifm-0.10
   echo vifm can not be installed
   exit 1
 fi
@@ -208,6 +237,7 @@ echo Install vimb
 wget "https://github.com/fanglingsu/vimb/archive/3.3.0.tar.gz"
 echo "5c6fe39b1b2ca18a342bb6683f7fd5b139ead53903f57dd9eecd5a1074576d6c  3.3.0.tar.gz" | sha256sum -c | grep -v OK
 if [ $? -eq 0 ]; then
+  rm 3.3.0.tar.gz
   echo vimb can not be loaded
   exit 1
 fi
@@ -219,6 +249,8 @@ cd vimb-3.3.0
 make -j4
 checkinstall -D -y --pkgname=vimb-ch --pkgversion=3.3.0 --nodoc --backup=no
 if [ $? -ne 0 ]; then
+  cd ..
+  rm -rf vimb-3.3.0
   echo vimb can not be installed
   exit 1
 fi
