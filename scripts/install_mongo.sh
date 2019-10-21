f0a93e2c50dee09b23dcf6a38d8f8f67714122ca
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-10-21 15:48:33 +0300

    fix problem with installation mongo

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -11,8 +11,8 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt-get update
 apt-get install -y \
   mongodb-org
