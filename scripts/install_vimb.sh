7894210d7462f1a5ea1a5325838281116a52fae2
andrejlevkovitch <andrejlevkovitch@gmail.com>
2020-02-13 17:17:08 +0300

    fix bug with installation vimb

--- scripts/install_vimb.sh
+++ scripts/install_vimb.sh
@@ -18,19 +18,20 @@ PACKAGE=vimb
 VERSION=3.3.0
 LINK="https://github.com/fanglingsu/vimb/archive/3.3.0.tar.gz"
 SHA_SUM="5c6fe39b1b2ca18a342bb6683f7fd5b139ead53903f57dd9eecd5a1074576d6c"
-ARCHIVE=vimb_ar
-OUT_DIR=vimb_dir
+ARCHIVE=$TMP_DIR/vimb_ar
+OUT_DIR=$TMP_DIR/vimb_dir
 
 check_package $PACKAGE
 if [ $? -ne 0 ]; then
   print_info "Install $PACKAGE"
 
   package_loader $LINK $ARCHIVE $SHA_SUM
-  if [ $? - ne 0 ]; then
+  if [ $? -ne 0 ]; then
     print_error "Can not load $PACKAGE"
     exit 1
   fi
 
+  mkdir $OUT_DIR
   tar -xzvf $ARCHIVE --directory $OUT_DIR --strip-components=1
   rm $ARCHIVE
   cd $OUT_DIR
