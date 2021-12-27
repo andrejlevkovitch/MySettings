5128542c367a9a6609b138c0d894313d49a5d835
andrejlevkovitch <andrejlevkovitch@gmail.com>
2021-12-27 13:01:59 +0100

    some change in cmake/version.cmake

--- tools/cmake/version.cmake
+++ tools/cmake/version.cmake
@@ -3,7 +3,7 @@
 
 find_package(Git REQUIRED)
 execute_process(
-  COMMAND           ${GIT_EXECUTABLE} describe --tags
+  COMMAND           ${GIT_EXECUTABLE} describe --tags --match "v*"
   RESULT_VARIABLE   IS_OK
   OUTPUT_VARIABLE   PROJECT_TAG
   WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
@@ -18,3 +18,4 @@ if(NOT IS_OK EQUAL 0)
 endif()
 
 STRING(STRIP ${PROJECT_TAG} PROJECT_VERSION)
+message(STATUS "PROJECT_VERSION: ${PROJECT_VERSION}")
