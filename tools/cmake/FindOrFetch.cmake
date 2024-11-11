9df50d98fd4d5e8e44442bbf5af6f17f6b265195
andrejlevkovitch <andrejlevkovitch@gmail.com>
2024-11-11 13:24:51 +0100

    add FindOrFetch command

---
+++ tools/cmake/FindOrFetch.cmake
@@ -0,0 +1,20 @@
+include(FetchContent)
+
+function(FindOrFetch name fetch_link)
+  if(TARGET ${name})
+    message(WARNING "use high level ${name} target")
+  else()
+    find_package(${name} QUIET)
+    if (${name}_FOUND)
+      message(STATUS "Found ${name}")
+    else()
+      message(WARNING "${name} was not found, try to fetch it from github")
+      string(REGEX MATCH "\/([^/]*)$" src_dir ${fetch_link})
+      FetchContent_Declare(${name}
+        GIT_REPOSITORY ${fetch_link}
+        SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/${src_dir}
+        )
+      FetchContent_MakeAvailable(${name})
+    endif()
+  endif()
+endfunction()
